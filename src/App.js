9d26619c7ac5a859dd9748e660fb1ef20cc1d13e
Ryan Tay Zhan En <awehouse@MacBook-Pro-2.local>
2019-06-30 15:34:10 +0800

    Added basic code for React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
 
 const Title = styled.h1`
   color: red;
-`
+`;
 
 const App = () => {
   return (
-    <div className="App">
-      <Title>
-        Hello World
-      </Title>
-
-
-    </div>
+    <Router>
+      <Title>Hello World</Title>
+      <Link to="/">Home</Link>
+      <Link to="/about/">About</Link>
+      <Route path="/" exact component={Home} />
+      <Route path="/about/" exact component={About} />
+    </Router>
   );
-}
+};
 
 export default App;
