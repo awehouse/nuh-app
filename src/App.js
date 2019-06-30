318ca1fe8a4c821856723b2ef7c6217fa9ce4484
Ryan Tay Zhan En <awehouse@MacBook-Pro-2.local>
2019-06-30 15:16:09 +0800

    Added styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+`
 
 const App = () => {
   return (
     <div className="App">
+      <Title>
+        Hello World
+      </Title>
 
 
-    
     </div>
   );
 }
